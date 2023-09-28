d65ea2f9e3dd461181a21517e5b58b46c9147c82
Anuj Shrestha <anujmani.shrestha@gmail.com>
2023-09-28 17:37:50 +0545

    Shoping page logic

---
+++ src/app/pages/shoppage/shoppage.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Pets , pets} from '../../pets'
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-shoppage',
+  templateUrl: './shoppage.component.html',
+  styleUrls: ['./shoppage.component.css']
+})
+export class ShoppageComponent implements OnInit {
+  pet = [...pets];
+  constructor(private route: ActivatedRoute){}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+
+}
